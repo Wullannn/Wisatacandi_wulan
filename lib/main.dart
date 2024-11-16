7192687688ed0d1c97904db2a0d680c64c0cfc1a
Wullannn <komariahwulandari_2226240119@mhs.mdp.ac.id>
2024-11-16 09:19:12 +0700

    pertemuan 19
    favorite_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wisatacandi_wulan/screens/favorite_screen.dart';
 import 'package:wisatacandi_wulan/screens/home_screen.dart';
 import 'package:wisatacandi_wulan/screens/search_screen.dart';
- import 'package:wisatacandi_wulan/screens/detail_screen.dart';
- import 'package:wisatacandi_wulan/data/candi_data.dart';
- import 'package:wisatacandi_wulan/screens/profile_screen.dart';
+import 'package:wisatacandi_wulan/screens/detail_screen.dart';
+import 'package:wisatacandi_wulan/data/candi_data.dart';
+import 'package:wisatacandi_wulan/screens/profile_screen.dart';
 import 'package:wisatacandi_wulan/screens/sign_in_screen.dart';
 import 'package:wisatacandi_wulan/screens/sign_up_screen.dart';
 
@@ -38,8 +39,80 @@ class MyApp extends StatelessWidget {
       // home: DetailScreen(candi: candiList[0]),
       // home: SignInScreen(),
       // home: SignUpScreen(),
-       home: SearchScreen(),
+      //  home: SearchScreen(),
       // home: HomeScreen(),
+      home: MainScreen(),
+    );
+  }
+}
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // TODO: 1. Deklarasikan variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO: 2. Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+      // TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+        // TODO: 4. Buat data dan Child dari Theme
+        data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[50]),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.deepPurple,
+              ),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.deepPurple,
+              ),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+              ),
+              label: 'Person',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showSelectedLabels: true,
+        ),
+      ),
     );
   }
 }
